0a4b6aab21d6d4bb8871a2f36a6d8a957d59de9f
Gene Haas <gene.haas1@gmail.com>
2018-07-22 00:17:34 -0500

    Add project files.

---
+++ MultiBinding/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MultiBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private decimal amount;
+
+        public decimal Amount
+        {
+            get { return amount; }
+            set { amount = value;
+            OnPropertyChanged("Amount");
+            }
+        }
+        private decimal discount;
+
+        public decimal Discount
+        {
+            get { return discount; }
+            set { discount = value;
+            OnPropertyChanged("Discount");
+            }
+        }
+        
+        
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string txt)
+        {
+
+            PropertyChangedEventHandler handle = PropertyChanged;
+            if (handle != null)
+            {
+                handle(this, new PropertyChangedEventArgs(txt));
+            }
+        }
+        #endregion
+    }
+}
---
+++ MultiBinding/TotalConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MultiBinding
+{
+   public class TotalConverter : IMultiValueConverter
+    {
+
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            decimal Amount = 0;
+            decimal Discount = 0;
+            string TotalAmount = string.Empty;
+            Amount = (values[0] != null && values[0] != DependencyProperty.UnsetValue) ? System.Convert.ToDecimal(values[0]) : 0;
+            Discount = (values[0] != null && values[1] != DependencyProperty.UnsetValue) ? System.Convert.ToDecimal(values[1]) : 0;
+            TotalAmount = System.Convert.ToString(Amount - Discount);
+          return TotalAmount; 
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
